211595711e9d7887207e68f827c24ff3f1d1bc4f
Marcelo Hadad <marcelo.hadad@outlook.com>
2020-12-29 05:11:17 -0300

    Configurando o banco - EFCore e Context

--- JobSearch.API/Startup.cs
+++ JobSearch.API/Startup.cs
@@ -27,8 +27,8 @@ namespace JobSearch.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<JobSearchContext>(options=> {
-                options.UseSqlite("Data Source=Database\\JobSearch.db");
+            services.AddDbContext<JobSearchContext>(options=> {                
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
         }
